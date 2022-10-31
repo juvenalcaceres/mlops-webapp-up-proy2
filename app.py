57f9dcd9f92cff63776efc1ca1d69803fb65434b
joenvihe <jorge.vicente@rimac.com.pe>
2022-10-31 05:07:51 -0500

    add error 3

--- app.py
+++ app.py
@@ -132,5 +132,7 @@ def predict_api(data_json):
 
 
 if __name__ =='__main__':
-    app.run(host ='0.0.0.0', port = port ,debug = True)
+    app.debug = True
+    app.run()
+    #app.run(host ='0.0.0.0', port = port ,debug = True)
 #########################################################################
